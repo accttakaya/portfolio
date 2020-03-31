6c7775e2ea979e903aebc4a9d56eeaa2a7aee16f
accttakaya <takaya.satou.0206@gmail.com>
2020-03-31 17:06:06 +0900

    お問い合わせフォーム、SNSなど

---
+++ contact.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+  $('#form').submit(function (event) {
+    var formData = $('#form').serialize();
+    $.ajax({
+      url: "https://docs.google.com/forms/u/2/d/e/1FAIpQLSd_abLT2-Cal_fuz7efyNoY9C8jCxOFZwHQpBI1SYF815K8_w/formResponse",
+      data: formData,
+      type: "POST",
+      dataType: "xml",
+      statusCode: {
+        0: function () {
+          // $(".end-message").slideDown();
+          // $(".submit-btn").fadeOut();
+          window.location.href = "thanks.html";
+        },
+        200: function () {
+          $(".false-message").slideDown();
+        }
+      }
+    });
+    event.preventDefault();
+  });
+
+});
