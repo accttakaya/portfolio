1dcd25384ce2cfa948cc4dda272c408b350cdc71
accttakaya <takaya.satou.0206@gmail.com>
2020-04-02 20:39:53 +0900

    topページの実装

---
+++ top.js
@@ -0,0 +1,20 @@
+$(function () {
+  
+
+  $('.impact-back-web').fadeIn(2000);
+
+  $('.impact-back-acct').delay(2000).fadeIn(2000);
+  // $('#simtax-btn').click(function() {
+  //   $('#simtax-modal-wrapper').fadeIn();
+  // });
+
+  // $('#furima-btn').click(function() {
+  //   $('#furima-modal-wrapper').fadeIn();
+  // });
+
+  // $('.close-btn').click(function() {
+  //   $('#simtax-modal-wrapper').fadeOut();
+  //   $('#furima-modal-wrapper').fadeOut();
+  // })
+
+});
