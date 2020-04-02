2b78e17fbeaf4694a5a5611f440043876d96d4b6
accttakaya <takaya.satou.0206@gmail.com>
2020-04-02 20:52:55 +0900

    不要削除

--- top.js
+++ top.js
@@ -1,20 +1,4 @@
 $(function () {
-  
-
   $('.impact-back-web').fadeIn(2000);
-
   $('.impact-back-acct').delay(2000).fadeIn(2000);
-  // $('#simtax-btn').click(function() {
-  //   $('#simtax-modal-wrapper').fadeIn();
-  // });
-
-  // $('#furima-btn').click(function() {
-  //   $('#furima-modal-wrapper').fadeIn();
-  // });
-
-  // $('.close-btn').click(function() {
-  //   $('#simtax-modal-wrapper').fadeOut();
-  //   $('#furima-modal-wrapper').fadeOut();
-  // })
-
 });
